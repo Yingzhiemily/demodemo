8c4a9ec2d395f8e211e88ff2445a1bd7318d2dc1
Yingzhiemily <emilyyingzhi@gmail.com>
2022-10-13 08:44:04 +0300

    submit

---
+++ todo/server/config.js
@@ -0,0 +1,11 @@
+const config = {
+    database: { 
+        host: 'localhost',
+        port:3309,
+        user: 'root',
+        password: 'root',
+        database: 'todo'
+    }
+}
+
+module.exports = config;
